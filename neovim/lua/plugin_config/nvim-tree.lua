59b6bd0310c363a02286bb6c3bada3751b964ffc
nicklcy <1787470600@qq.com>
2022-08-18 12:23:18 +0800

    update neovim

--- neovim/lua/plugin_config/nvim-tree.lua
+++ neovim/lua/plugin_config/nvim-tree.lua
@@ -1,18 +1,13 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-require'nvim-tree'.setup {
+require('nvim-tree').setup {
   disable_netrw       = true,
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = false,
     icons = {
@@ -45,7 +40,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {}
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -1,63 +1,63 @@
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-treesitter/playground',
-    run = ':TSUpdate'
-  }
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		'nvim-treesitter/playground',
+		run = ':TSUpdate'
+	}
 
-  use 'shaunsingh/nord.nvim'
+	use 'shaunsingh/nord.nvim'
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {
-      'kyazdani42/nvim-web-devicons', opt = true
-    }
-  }
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = {
+			'kyazdani42/nvim-web-devicons', opt = true
+		}
+	}
 
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    }
-  }
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons', -- optional, for file icon
+		}
+	}
 
-  use {
-    'akinsho/bufferline.nvim',
-    requires = {
-      'kyazdani42/nvim-web-devicons'
-    }
-  }
+	use {
+		'akinsho/bufferline.nvim',
+		requires = {
+			'kyazdani42/nvim-web-devicons'
+		}
+	}
 
-  use {
-    'numToStr/Comment.nvim',
-  }
+	use {
+		'numToStr/Comment.nvim',
+	}
 
-  use {
-    'phaazon/hop.nvim',
-    -- branch = 'v1', -- optional but strongly recommended
-  }
+	use {
+		'phaazon/hop.nvim',
+		-- branch = 'v1', -- optional but strongly recommended
+	}
 
-  use {
-    'neovim/nvim-lspconfig',
-    'hrsh7th/nvim-cmp',
-    'folke/lua-dev.nvim',
-    'hrsh7th/cmp-nvim-lsp'
-  }
+	use {
+		'neovim/nvim-lspconfig',
+		'hrsh7th/nvim-cmp',
+		'folke/lua-dev.nvim',
+		'hrsh7th/cmp-nvim-lsp'
+	}
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    }
-  }
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {
+			'nvim-lua/plenary.nvim'
+		}
+	}
 
-  use {
-    "akinsho/toggleterm.nvim"
-  }
+	use {
+		"akinsho/toggleterm.nvim"
+	}
 
 	use {
 		"jlanzarotta/bufexplorer"
@@ -65,10 +65,10 @@ require('packer').startup(function()
 end)
 
 plugin_files = {'treesitter', 'nord', 'lualine', 'nvim-tree',
-  'bufferline', 'Comment', 'hop', 'lspconfig', 'cmp',
-  'toggleterm'}
+'bufferline', 'Comment', 'hop', 'lspconfig', 'cmp',
+'toggleterm'}
 for i = 1, #plugin_files do
-  plugin = plugin_files[i]
-  print('Load plugin config file: '..plugin)
-  require('plugin_config/'..plugin)
+	plugin = plugin_files[i]
+	-- print('Load plugin config file: '..plugin)
+	require('plugin_config/'..plugin)
 end
