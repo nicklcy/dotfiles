4906d2c9ea4c0f20d9906216a63dd44c75b0d468
nicklcy <1787470600@qq.com>
2022-09-11 18:27:04 +0800

    add i3 screenshot shortcut - $mod+Shift+p

--- systemd/shell/low-battery-notification.sh
+++ systemd/shell/low-battery-notification.sh
@@ -3,7 +3,7 @@
 
 printf 'script started' | systemd-cat -t low-battery-notification #write to log when script is called
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -f 1 -d "%" ` < 18 ]] ; then
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -f 1 -d "%" ` < 20 ]] ; then
         printf 'condition is true' | systemd-cat -t low-battery-notification #write to log if condition is true
         /usr/bin/notify-send -u low "Battery Low" "$BATTINFO"
 fi
