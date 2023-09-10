333a814a42253572b2c3ef4e3fc8cc8e50059c6e
nickluocy <1787470600@qq.com>
2023-09-10 16:06:38 +0800

    update xf86 key bindings and nvim-tree

--- neovim/lua/plugin_config/nvim-tree.lua
+++ neovim/lua/plugin_config/nvim-tree.lua
@@ -37,10 +37,6 @@ require('nvim-tree').setup {
     width = 30,
     hide_root_folder = false,
     side = 'left',
-    mappings = {
-      custom_only = false,
-      list = {}
-    },
     number = false,
     relativenumber = false,
     signcolumn = "yes"
