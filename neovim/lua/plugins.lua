237f2268b9a31f7bc5a989fa3ddec04cf616b13b
nickluocy <1787470600@qq.com>
2024-04-30 14:35:22 +0800

    udpate nvim - change from packer.nvim to lazy.nvim

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -1,87 +1,53 @@
-vim.cmd [[packadd packer.nvim]]
+# bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-
-	use {
+plugins = {
+	{
 		'nvim-treesitter/nvim-treesitter',
 		'nvim-treesitter/playground',
-		run = ':TSUpdate'
-	}
-
-	use 'shaunsingh/nord.nvim'
-
-	use 'kyazdani42/nvim-web-devicons'
-
-	use 'nvim-lualine/lualine.nvim'
-
-	use 'kyazdani42/nvim-tree.lua'
-
-	use 'akinsho/bufferline.nvim'
-
-	use {
-		'numToStr/Comment.nvim',
-	}
-
-	use {
-		'phaazon/hop.nvim',
-		branch = 'v2', -- optional but strongly recommended
-	}
-
-	-- use {
-	-- 	'neovim/nvim-lspconfig',
-	-- 	'hrsh7th/nvim-cmp',
-	-- 	'hrsh7th/cmp-nvim-lua',
-	-- 	'hrsh7th/cmp-nvim-lsp',
-	-- 	'hrsh7th/cmp-nvim-lsp-signature-help',
-	-- 	'folke/neodev.nvim',
-	-- 	'onsails/lspkind.nvim',
-	-- 	'saadparwaiz1/cmp_luasnip'
-	-- }
-
-	use({
-		'neoclide/coc.nvim', branch = 'release',
-		requires = {
+	},
+	'shaunsingh/nord.nvim',
+	'kyazdani42/nvim-web-devicons',
+	'nvim-lualine/lualine.nvim',
+	'kyazdani42/nvim-tree.lua',
+	'akinsho/bufferline.nvim',
+	'numToStr/Comment.nvim',
+	{ 'phaazon/hop.nvim', branch = 'v2' },
+  { 'neoclide/coc.nvim', branch = 'release',
+		dependencies = {
 			'rafamadriz/friendly-snippets'
 		}
-	})
-
-	use({
-		"L3MON4D3/LuaSnip",
-		-- tag = "v<CurrentMajor>.*",
-	})
-
-	use {
+  },
+	"L3MON4D3/LuaSnip",
+	{
 		'nvim-telescope/telescope.nvim',
-		requires = {
+		dependencies = {
 			'nvim-lua/plenary.nvim'
 		}
-	}
+	},
+	"akinsho/toggleterm.nvim",
+	"jlanzarotta/bufexplorer",
+	'lervag/vimtex',
+  'Vonr/align.nvim',
+	'lambdalisue/suda.vim',
+}
 
-	use {
-		"akinsho/toggleterm.nvim"
-	}
-
-	use {
-		"jlanzarotta/bufexplorer"
-	}
-
-	use {
-		'lervag/vimtex'
-	}
-
-  use {
-    'Vonr/align.nvim',
-    branch = "v2"
-  }
-
-	use 'lambdalisue/suda.vim'
-end)
+require("lazy").setup(plugins)
 
 local plugin_files = { 'nord', 'lualine', 'nvim-tree',
 	'bufferline', 'Comment', 'hop', 'toggleterm',
 	'treesitter', 'vimtex', 'align',
-	--'lspconfig', 'cmp',
 	'coc-nvim' }
 for i = 1, #plugin_files do
 	local plugin = plugin_files[i]
