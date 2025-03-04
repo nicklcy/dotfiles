14e2ade38d4d1521d1d21bfe7ade8493abb7d265
nickluocy <1787470600@qq.com>
2025-03-04 15:48:37 +0800

    update neovim debugger and add key remaps in hyprland

--- neovim/lua/plugin_config/dap.lua
+++ neovim/lua/plugin_config/dap.lua
@@ -11,5 +11,6 @@ local NS = { noremap = true, silent = true }
 keyset("n", "<Leader>dbg", dapui.toggle, NS)
 keyset("n", "<Leader>dbb", dap.toggle_breakpoint, NS)
 keyset("n", "<Leader>dbc", dap.continue, NS)
+keyset("n", "<Leader>dbr", dap.run_last, NS)
 keyset("n", "<Leader>dbso", dap.step_over, NS)
 keyset("n", "<Leader>dbsi", dap.step_into, NS)
